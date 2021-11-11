4ee7b263f18282d75b57960e4ec6d5a57646d5dc
ehp2021 <emmahpaik@gmail.com>
2021-11-11 16:25:14 -0600

    try again

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const importData = require('./db.json');
 let port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
+// app.get("/", (req, res) => {
+//     res.send("hello world");
+// });
 
 app.get("/doctors", (req, res) => {
     res.send(importData);

9809027cbf1dca1dd20077a82381ccb676033e3c
ehp2021 <emmahpaik@gmail.com>
2021-11-11 17:14:32 -0600

    with cors

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const importData = require('./db.json');
 let port = process.env.PORT || 5000;
 
-// app.get("/", (req, res) => {
-//     res.send("hello world");
-// });
-
+app.use(cors())
 app.get("/doctors", (req, res) => {
     res.send(importData);
 });
